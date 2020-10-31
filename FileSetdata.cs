6ecd01cb2a56ca74c4ff0fd9b42ed991eb3f2142
石泉 慎一 <rrmkk086@yahoo.co.jp>
2020-10-31 20:50:59 +0900

    プロジェクト ファイルを追加します。

---
+++ FileSetdata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSetdata
+{
+    public class FileSetdata
+    {
+        public string FileName { get; private set; }
+        public string FolderName { get; private set; }
+        public string Extension { get; private set; }
+
+        public FileSetdata() { }
+        public FileSetdata(string filename, string foldername, string extension)
+        {
+            FileName = filename;
+            FolderName = foldername;
+            Extension = extension;
+        }
+    }
+}
